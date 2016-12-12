01ba6e44c35ce934a188916a9882d60101b8865a
Reena-jacob <reenajacob.mec@gmail.com>
2016-12-12 23:39:01 +0530

    Add files via upload

---
+++ k8.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+import math
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_POS_MSEC,100000)
+success,img = cap.read()
+if  success:
+    cv2.rectangle(img,(300,300),(100,100),(0,255,0),0)
+    crop_img = img[100:300, 100:300]
+    grey = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
+    value = (35, 35)
+    blurred = cv2.GaussianBlur(grey, value, 0)
+    _, thresh1 = cv2.threshold(blurred, 127, 255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    cv2.imshow('Thresholded', thresh1)
+    image, contours, hierarchy = cv2.findContours(thresh1.copy(),cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    cnt = max(contours, key = lambda x: cv2.contourArea(x))
+    x,y,w,h = cv2.boundingRect(cnt)
+    cv2.rectangle(crop_img,(x,y),(x+w,y+h),(0,0,255),0)
+    hull = cv2.convexHull(cnt)
+    drawing = np.zeros(crop_img.shape,np.uint8)
+    cv2.drawContours(drawing,[cnt],0,(0,255,0),0)
+    cv2.drawContours(drawing,[hull],0,(0,0,255),0)
+    hull = cv2.convexHull(cnt,returnPoints = False)
+    cv2.imshow('Gesture', img)
+    all_img = np.hstack((drawing, crop_img))
+    cv2.imshow('Contours', all_img)
+    cv2.imwrite("f20sec1.jpg", all_img)
+   
+
+
+
